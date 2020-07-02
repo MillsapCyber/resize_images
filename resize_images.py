9fec189ad5ec7020f86e5132be76ada178c949e4
Josh Millsap <22407981+MillsapCyber@users.noreply.github.com>
2020-07-01 21:17:14 -0700

    Add support for jpg / png

--- resize_images.py
+++ resize_images.py
@@ -2,16 +2,34 @@ import os, re, argparse
 from PIL import Image
 
 
-def resize(path):
+def resize(path, type):
     dirs = os.listdir(path)
     if not os.path.exists(path + 'resized'):
         os.mkdir(path + 'resized')
+    if type == 'png':
+        func = resize_png
+    elif type == 'jpg':
+        func = resize_jpg
+    else:
+        exit(-1)
+
     for item in dirs:
         if os.path.isfile(path + item):
             im = Image.open(path + item)
-            im_resize = im.resize((args.width, args.height), Image.ANTIALIAS)
-            p = path + 'resized/' + re.search('(.+?)(\.[^.]*$|$)', item).group(1) + '.png'
-            im_resize.save(p, 'PNG', quality=90)
+            func(im, item, path)
+
+
+def resize_png(im, item, path):
+    im_resize = im.resize((args.width, args.height), Image.ANTIALIAS)
+    p = path + 'resized/' + re.search('(.+?)(\.[^.]*$|$)', item).group(1) + '.png'
+    im_resize.save(p, 'PNG', quality=90)
+
+
+def resize_jpg(im, item, path):
+    im = im.convert("RGB")
+    im_resize = im.resize((args.width, args.height), Image.ANTIALIAS)
+    p = path + 'resized/' + re.search('(.+?)(\.[^.]*$|$)', item).group(1) + '.jpg'
+    im_resize.save(p, 'JPEG', quality=90)
 
 
 if __name__ == '__main__':
@@ -23,6 +41,9 @@ if __name__ == '__main__':
                         required=False, help='width you want to resized image to be')
     parser.add_argument('-y', '--height', type=int, default=500, metavar='',
                         required=False, help='height you want to resized image to be')
+    parser.add_argument('-t', '--type', type=str, default='png', metavar='',
+                        required=False, help='type of image you want (png/jpg)')
+
     args = parser.parse_args()
     # replace \'s with /'s for windows ppl
     p = args.path.replace("\\", "/")
@@ -30,4 +51,4 @@ if __name__ == '__main__':
     if p[-1] != "/":
         p = p + "/"
 
-    resize(p)
+    resize(p, args.type)
